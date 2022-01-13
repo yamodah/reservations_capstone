b039666bc70d22627f6cfea9c91b60e2340eb475
yamodah <yassinedahlek@gmail.com>
2022-01-13 16:15:25 -0500

    added form useState

--- front-end/src/layout/NewReservation.js
+++ front-end/src/layout/NewReservation.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState }  from 'react'
 
 function NewReservation() {
+    const [form, setform] = useState({...initialFormState})
+    const initialFormState = {
+        first_name:"",
+        last_name:"",
+        mobile_number:"",
+        reservation_date:"",
+        reservation_time:"",
+        people:"",
+    }
+
     return (
         <div>
-            
+
         </div>
     )
 }
