74be852c90098b25f8572948112a22fbe0a87efb
yamodah <yassinedahlek@gmail.com>
2022-01-13 16:41:17 -0500

    added unsigned() to my integer columns

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
     table.string("mobile_number");
     table.date("reservation_date");
     table.time("reservation_time");
-    table.integer("people")
+    table.integer("people").unsigned()
     table.string("status").notNullable.defaultTo("booked")
     table.timestamps(true, true);
   });
