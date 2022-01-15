5977a7c830f5bca38a7c1647d92c4da914f7ee35
yamodah <yassinedahlek@gmail.com>
2022-01-15 11:55:43 -0500

    named functions in reservations.service no knex logic yet

---
+++ back-end/src/reservations/reservations.service.js
@@ -0,0 +1,33 @@
+const knex = require("../db/connection")
+
+const list =()=>{
+    
+}
+const listByDate =(date)=>{
+
+}
+const listByPhoneNumber =(mobile_number)=>{
+
+}
+const read =()=>{
+
+}
+const create =()=>{
+
+}
+const update =()=>{
+
+}
+const destroy =()=>{
+
+}
+
+module.exports={
+    list,
+    listByDate,
+    listByPhoneNumber,
+    read,
+    update,
+    destroy,
+    create
+}
