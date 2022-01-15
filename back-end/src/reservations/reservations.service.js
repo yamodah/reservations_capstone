bfa1ae113f8fb511ad81fa0f1ed69489fe7e1d1a
yamodah <yassinedahlek@gmail.com>
2022-01-15 12:15:37 -0500

    added almost all logic for service file

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,25 +1,44 @@
 const knex = require("../db/connection")
 
 const list =()=>{
-    
+    return knex("reservations")
+        .select("*")
+        .orderBy("reservation_date")
 }
-const listByDate =(date)=>{
-
+const listByDate =(reservation_date)=>{
+    return knex("reservations")
+        .select("*")
+        .where({reservation_date})
 }
-const listByPhoneNumber =(mobile_number)=>{
-
+const listByMobileNumber =(mobile_number)=>{
+    return knex("reservations")
+        .select("*")
+        .where({mobile_number})
 }
-const read =()=>{
-
+const read =(reservation_id)=>{
+    return knex("reservations")
+        .select("*")
+        .where({reservation_id})
+        .first()
 }
-const create =()=>{
-
+const create =(newReservation)=>{
+    return knex("reservations")
+        .insert(newReservation)
+        .returning("*")
+        .then((reservation)=>reservation[0])
 }
-const update =()=>{
-
+const update =({reservation_id})=>{
+    return knex("reservations")
+        .select("*")
+        .where({reservation_id})
+        .update(updatedworkout)
+        .returning("*")
+        .then((workout)=>workout[0])
 }
-const destroy =()=>{
-
+const destroy =(reservation_id)=>{
+    return knex("reservations")
+        .where({reservation_id})
+        .del()
 }
 
 module.exports={
