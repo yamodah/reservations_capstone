9d111572e137b8906dea93eee4254d8cdea2aa6e
yamodah <yassinedahlek@gmail.com>
2022-01-16 21:41:36 -0500

    built out basic validation functions for reservation properties

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,71 +1,76 @@
-const service = require("./reservations.service")
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
-const {hasProperties, hasOnlyValidProperties} = require("../errors/hasProperties")
+const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const {
+  hasProperties,
+  hasOnlyValidProperties,
+} = require("../errors/hasProperties");
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  const {date} = req.query
-  const {mobile_number} = req.query
-  let data
-  if(date){
-    data = await service.listByDate(date)
-  }else if(mobile_number){
-    data = await service.listByMobileNumber(mobile_number)
-  }else{
-    data = await service.list()
+  const { date } = req.query;
+  const { mobile_number } = req.query;
+  let data;
+  //logic to handle optional query parameters for dashboard
+  if (date) {
+    data = await service.listByDate(date);
+  } else if (mobile_number) {
+    data = await service.listByMobileNumber(mobile_number);
+  } else {
+    data = await service.list();
   }
-res.json({data})
+  res.json({ data });
 }
 
 async function read(req, res) {
-  res.json({data: await service.read(res.locals.reservation.reservation_id)})
+  res.json({ data: await service.read(res.locals.reservation.reservation_id) });
 }
 async function create(req, res) {
-const data = await service.create(res.locals.reservation)
-res.json({data})
+  const data = await service.create(res.locals.reservation);
+  res.json({ data });
 }
-async function update(req, res){
-  const {reservation_id} =res.locals.reservation
+async function update(req, res) {
+  const { reservation_id } = res.locals.reservation;
   const editedReservation = {
-      ...req.body,
-      reservation_id
-  }
-  const data = await service.update(editedReservation)
-  res.json({data})
+    ...req.body,
+    reservation_id,
+  };
+  const data = await service.update(editedReservation);
+  res.json({ data });
 }
 async function destroy(req, res) {
-  await service.destroy(res.locals.reservation.reservation_id)
-  res.sendStatus(201)
+  await service.destroy(res.locals.reservation.reservation_id);
+  res.sendStatus(201);
 }
-//VALIDATION FUNCTIONS
 
-const reservationExists = async (req,res,next)=>{
-  const {reservation_id}=req.params
-  const reservation = await service.read(reservation_id)
-  if(reservation){
+//======VALIDATION FUNCTIONS========//
+
+const reservationExists = async (req, res, next) => {
+  const { reservation_id } = req.params;
+  const reservation = await service.read(reservation_id);
+  if (reservation) {
     //stores found reservation in locals object to remove the need for unnecessary queries in the future
-    res.locals.reservation = reservation
-    next()
-  }else{
+    res.locals.reservation = reservation;
+    next();
+  } else {
     next({
-      status:404,
-      message:`Sorry no reservation found with id:${reservation_id}`
-    })
+      status: 404,
+      message: `Sorry no reservation found with id:${reservation_id}`,
+    });
   }
-}
+};
 //checking is data is even present or if the request is empty
-const hasPayload = (req,res,next)=>{
-  const data = req.body.data
-  if(!data){
+const hasPayload = (req, res, next) => {
+  const data = req.body.data;
+  if (!data) {
     next({
-      status:400,
-      message:"Data is required for a valid request"
-    })
-  }else{
-    next()
+      status: 400,
+      message: "Data is required for a valid request",
+    });
+  } else {
+    next();
   }
-}
+};
 //Named to work with hasOnlyValidProperties function
 const VALID_PROPERTIES = [
   "first_name",
@@ -73,45 +78,88 @@ const VALID_PROPERTIES = [
   "mobile_number",
   "reservation_date",
   "reservation_time",
-  "people"
+  "people",
 ];
-const requiredFieldsCheck = hasProperties(...VALID_PROPERTIES)
-const dateValidation = (req,res,next)=>{
-  const { reservation_date } = req.bod.data
-  const today = new Date()
-  const reservationDate = new Date(reservation_date)
-  const dateFormat = /\d\d\d\d-\d\d-\d\d/
-  if(!reservation_date.match(dateFormat)){
+const requiredFieldsCheck = hasProperties(...VALID_PROPERTIES);
+const dateValidation = (req, res, next) => {
+  const { reservation_date } = req.bod.data;
+  const today = new Date();
+  const reservationDate = new Date(reservation_date);
+  //regex to match only digits in YYYY-MM-DD format
+  const dateFormat = /\d\d\d\d-\d\d-\d\d/;
+
+  //in case a reservation needs to changed the same day
+  if (res.locals.reservation) return next();
+
+  if (!dateFormat.test(reservation_date)) {
     next({
-      status:400,
-      message:"Date must be submitted in 'YYYY-MM-DD' format."
-    })
-  }else if(reservationDate < today){
+      status: 400,
+      message: "Date must be submitted in 'YYYY-MM-DD' format.",
+    });
+  } else if (reservationDate < today) {
     next({
-      status:400,
-      message:"Reservations must be made at least a day in advance"
-    })
-  }else if(reservation_date.getDay()=== 2){
+      status: 400,
+      message: "Reservations must be made at least a day in advance",
+    });
+  } else if (reservation_date.getDay() === 2) {
     // '2' is the equivalent to Tuesday
     next({
+      status: 400,
+      message: "Sorry we are closed on Tuesdays",
+    });
+  } else {
+    next();
+  }
+};
+const timeValidation = (req, res, next) => {
+  const { reservation_time } = req.bod.data;
+  //regex to match time formats (only digits, and hours then minutes, etc.)
+  const timeFormat = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/;
+  if(!timeFormat.test(reservation_time)){
+    next({
       status:400,
-      message:"Sorry we are closed on Tuesdays"
+      message:"'time' must be submitted in 'HH:MM:SS' or 'HH:MM' format"
+    })
+  }else if( reservation_time< "10:30" || reservation_time > "21:30"){
+    next({
+      status:400,
+      message:"reservations must be made between 10:30AM  and 9:30PM"
     })
   }else{
     next()
   }
+};
+const peopleValidation = (req, res, next) => {
+  const { people } = req.bod.data;
+  //SQL column for poeple is for integers 
+  if(people<=0||typeof people !== "number"){
+    next({
+      status:400,
+      message:"'people' must be a NUMBER greater than 0"
+    })}else{
+      next()
+    }
 }
-const timeValidation = (req,res,next)=>{
-  const { people } = req.bod.data
-  const timeFormat = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/
-}
-const peopleValidation = (req,res,next)=>{
-const { people } = req.bod.data
-}
+;
 module.exports = {
   list,
-  read:[asyncErrorBoundary(reservationExists),read],
-  update:[asyncErrorBoundary(reservationExists),requiredFieldsCheck],
-  create:[hasPayload,requiredFieldsCheck,hasOnlyValidProperties,],
-  delete:[asyncErrorBoundary(reservationExists),destroy]
+  read: [asyncErrorBoundary(reservationExists), read],
+  update: [
+    asyncErrorBoundary(reservationExists),
+    requiredFieldsCheck,
+    dateValidation,
+    timeValidation,
+    peopleValidation,
+    asyncErrorBoundary(update)
+  ],
+  create: [
+    hasPayload,
+    requiredFieldsCheck,
+    hasOnlyValidProperties,
+    dateValidation,
+    timeValidation,
+    peopleValidation,
+    asyncErrorBoundary(create)
+  ],
+  delete: [asyncErrorBoundary(reservationExists), destroy],
 };
--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -6,7 +6,20 @@
 
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
+router
+  .route("/:reservationId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
+  .all(methodNotAllowed);
+
 
 module.exports = router;
