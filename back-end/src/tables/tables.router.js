87b4e466fa7ed287f7d6d1f9b2e492225316c736
yamodah <yassinedahlek@gmail.com>
2022-01-18 10:29:49 -0500

    built out tables.router

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,1 +1,17 @@
 const router = require("express").Router()
+const controller = require("./tables.controller")
+const methodNotAllowed = require("../errors/methodNotAllowed")
+
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
+router
+    .route("/:table_id/seat")
+    .put(controller.seatReservation)
+    .delete(controller.clear)
+    .all(methodNotAllowed)
+
+module.exports = router 
