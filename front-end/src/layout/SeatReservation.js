0ecde7fb132f24097a5394b3906b289d326769fa
yamodah <yassinedahlek@gmail.com>
2022-01-21 13:33:12 -0500

    added change handler

--- front-end/src/layout/SeatReservation.js
+++ front-end/src/layout/SeatReservation.js
@@ -1,9 +1,15 @@
 import React, {useState, useEffect} from 'react'
-import {listTables} from "../utils/api"
+import { useParams } from 'react-router-dom'
+import {listTables, readReservation} from "../utils/api"
+import ErrorAlert from './ErrorAlert'
 function SeatReservation() {
+    const {reservation_id} = useParams
     const [tables, setTables] = useState([])
+    const [chosenTable, setChosenTable]= useState({table_id:null})
     const [error, setError]= useState(null)
+    const [reservation, setReservation] = useState({})
     useEffect(loadTables,[])
+    useEffect(loadReservationData,[reservation_id])
     function loadTables(){
         const AC = new AbortController();
         listTables(AC.signal)
@@ -11,9 +17,36 @@ function SeatReservation() {
           .catch(setError);
         return () => AC.abort();
       }
+      function loadReservationData() {
+        const abortController = new AbortController();
+        setError(null);
+        readReservation(reservation_id, abortController.signal)
+          .then(setReservation)
+          .catch(setError);
+        return () => abortController.abort();
+      }
+
+    const tableOptions = tables.map((table)=>{
+        <option key={table.table_id} value={table.table_id}>
+            {table.table_name} - {table.capacity}
+        </option>
+    })
+    const handleChange = (e) => {
+        setChosenTable({
+          ...reservation,
+          [e.target.id]: e.target.value,
+        });
+      };
     return (
         <form>
-            
+            <ErrorAlert error={error}/>
+            <div class="form-group">
+                <label htmlFor="table_id">Choose a Table</label>
+                <select class="form-control" id="table_id" onChange={handleChange}>
+                    <option value="">** No Selection **</option>
+                    {tables.length && tableOptions}
+                </select>
+             </div>
         </form>
     )
 }
