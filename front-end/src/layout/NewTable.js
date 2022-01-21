be34097846bf09aada5db40a70e6168758c7d5b0
yamodah <yassinedahlek@gmail.com>
2022-01-21 13:08:56 -0500

    added logic for seatRes and removed console.log and renamed htmlFor on newtable

--- front-end/src/layout/NewTable.js
+++ front-end/src/layout/NewTable.js
@@ -14,7 +14,7 @@ function NewTable() {
         e.preventDefault()
         const AC = new AbortController()
         createTable(form,AC.signal)
-        // .then(()=>history.push(`/dashboard?date=${form.reservation_date}`))
+        .then(()=>history.push("/"))
         .catch(setError)
 
     }
@@ -29,17 +29,16 @@ function NewTable() {
           ...form,
           [e.target.id]: Number(e.target.value),
         });
-        // console.log( typeof Number(e.target.value))
       };
     return (
         <form onSubmit={handleSubmission}>
             <ErrorAlert error={error}/>
             <div className="mb-3">
-                <label htmlFor="formGroupExampleInput" className="form-label"  >Table Name</label>
+                <label htmlFor="table_name" className="form-label"  >Table Name</label>
                 <input type="text" className="form-control" id="table_name" name="table_name" placeholder="Table Name" value={form.table_name} onChange={handleChange} required/>
             </div>
             <div className="mb-3">
-                <label htmlFor="formGroupExampleInput2" className="form-label">Capacity</label>
+                <label htmlFor="capacity" className="form-label">Capacity</label>
                 <input type="number" className="form-control" id="capacity" name="capacity" placeholder="Capacity" value={form.capacity} onChange={handleNumberChange} required/>
             </div>
             <button onClick={(e)=>{
--- front-end/src/layout/SeatReservation.js
+++ front-end/src/layout/SeatReservation.js
@@ -1,10 +1,20 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import {listTables} from "../utils/api"
 function SeatReservation() {
+    const [tables, setTables] = useState([])
+    const [error, setError]= useState(null)
+    useEffect(loadTables,[])
+    function loadTables(){
+        const AC = new AbortController();
+        listTables(AC.signal)
+          .then(setTables)
+          .catch(setError);
+        return () => AC.abort();
+      }
     return (
-        <div>
+        <form>
             
-        </div>
+        </form>
     )
 }
 
