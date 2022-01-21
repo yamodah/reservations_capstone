b608036a523f2c5ab3a9d4dd4812191111d05c0c
yamodah <yassinedahlek@gmail.com>
2022-01-21 18:51:02 -0500

    added form comp and trying to make it modular

--- front-end/src/layout/NewReservation.js
+++ front-end/src/layout/NewReservation.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom'
 import {asDateString} from "../utils/date-time"
 import { createReservation } from "../utils/api.js"
 import ErrorAlert from './ErrorAlert'
+import ReservationForm from './ReservationForm'
 function NewReservation() {
     const date = new Date()
     const history = useHistory()
@@ -40,37 +41,7 @@ function NewReservation() {
 
       }
     return (
-        <form onSubmit={handleSubmission}>
-            <ErrorAlert error={error}/>
-            <div className="mb-3">
-                <label htmlFor="formGroupExampleInput" className="form-label"  >First Name</label>
-                <input type="text" className="form-control" id="first_name" name="first_name" placeholder="First Name" value={form.first_name} onChange={handleChange} required/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="formGroupExampleInput2" className="form-label">Last Name</label>
-                <input type="text" className="form-control" id="last_name" name="last_name" placeholder="Last Name" value={form.last_name} onChange={handleChange} required/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="formGroupExampleInput" className="form-label">Mobile Number</label>
-                <input type="text" className="form-control" id="mobile_number" name="mobile_number"placeholder="Mobile Number"  value={form.mobile_number} onChange={handleChange} required/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="formGroupExampleInput2" className="form-label">Date</label>
-                <input type="date" className="form-control" id="reservation_date" name="reservation_date"value={form.reservation_date} onChange={handleChange} required/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="formGroupExampleInput2" className="form-label">Time</label>
-                <input type="time" className="form-control" id="reservation_time" name="reservation_time" value={form.reservation_time} onChange={handleChange} required/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="formGroupExampleInput2" className="form-label">People</label>
-                <input type="number" className="form-control" id="people" name="people"value={form.people} onChange={handleNumberChange} min={1} required/>
-            </div>
-            <button onClick={(e)=>{
-                e.preventDefault()
-                history.goBack()}}>Cancel</button>
-            <button type="submit">Submit</button>
-        </form>
+        <ReservationForm handleSubmission={handleSubmission} handleChange={handleChange} error={error} handleNumberChange={handleNumberChange}/>
     )
 }
 
