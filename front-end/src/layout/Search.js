9afa3a01de2e2212686e7b6f3d439f65f799b197
yamodah <yassinedahlek@gmail.com>
2022-01-21 18:32:34 -0500

    user story 7 passing all tests

--- front-end/src/layout/Search.js
+++ front-end/src/layout/Search.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { listReservations } from '../utils/api';
+import ReservationsTable from './ReservationsTable'
 
 function Search() {
+    const [number, setNumber]=useState("")
+    const [reservations,setReservations]=useState([])
+    const handleChange = (e) => {
+        setNumber(e.target.value);
+      };
+    const handleSearch = (e)=> {
+        e.preventDefault()
+        const AC = new AbortController()
+        listReservations({mobile_number:number},AC.signal).then((setReservations)).then(()=>setNumber(""))
+          .catch(console.error)
+    }
     return (
         <div>
-            
+        <form onSubmit={handleSearch}>
+        <div className="mb-3">
+           <input id="mobile_number"type="text" value={number} onChange={handleChange} name="mobile_number"/>
+           <button type="Submit" className=''>Find</button>
+        </div>
+        </form>
+           <ReservationsTable reservations={reservations}/>
+           {!reservations.length && <h3>No reservations found</h3>}
         </div>
     )
 }
