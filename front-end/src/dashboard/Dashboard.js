38dc99163a9b774b9a2b57de0bf7e5fae85c8563
yamodah <yassinedahlek@gmail.com>
2022-01-21 10:36:17 -0500

    changed error alert for dashboard

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -11,44 +11,42 @@ import ErrorAlert from "../layout/ErrorAlert";
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
   const [tables, setTables] = useState([]);
-  const [reservationsError, setReservationsError] = useState(null);
-  const [tablesError, setTablesError] = useState(null);
+  const [error, setError] = useState(null);
+
 
   useEffect(loadDashboard, [date]);
   useEffect(loadTables, []);
 
   function loadDashboard() {
     const abortController = new AbortController();
-    setReservationsError(null);
+    setError(null);
     listReservations({ date }, abortController.signal)
       .then(setReservations)
-      .catch(setReservationsError);
+      .catch(setError);
     return () => abortController.abort();
   }
   function loadTables(){
     const AC = new AbortController();
     listTables(AC.signal)
       .then(setTables)
-      .catch(setTablesError);
+      .catch(setError);
     return () => AC.abort();
   }
   return (
     <main>
       <h1>Dashboard</h1>
+      <ErrorAlert error={error} />
       <div style={{display:"flex",flexDirection:"row"}}>
        <div>
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for {date}</h4>
       </div>
       {JSON.stringify(reservations)}
-      <ErrorAlert error={reservationsError} />
       </div> 
       <div>
-      
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Tables</h4>
       </div>
-      <ErrorAlert error={tablesError} />
       {JSON.stringify(tables)}
       </div>
       </div>
